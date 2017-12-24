48a353dabe2b7da7860d9e199b9faa561f02ec58
Adnan Chowdhury <adnanchowdhury88@gmail.com>
2017-12-23 18:30:50 -0800

    Get React into the mix

---
+++ src/Simulator.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default class Simulator extends React.Component {
+  render() {
+    return (
+      <h1>Hi</h1>
+    );
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,17 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Simulator from './Simulator';
 import './index.scss';
 
+/**
+ * Screen-blocking loading indicator removed on 'onload' event
+ */
 window.onload = () => {
-  const loadingIndicator = document.getElementsByClassName('loading-indicator')[0];
-  loadingIndicator.remove();
+  const loadingIndicator = document.getElementById('loading-indicator');
+  loadingIndicator && loadingIndicator.remove();
 };
+
+ReactDOM.render(
+  <Simulator />,
+  document.getElementById('root')
+);
